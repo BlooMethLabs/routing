d6effb45ba3e53daa9a16fa5a7796f10c48f4e55
BlooMethLabs <bmca620@gmail.com>
2020-11-28 16:53:11 +0000

    Passing route params

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 import Posts from '../../containers/Posts/Posts';
 import NewPost from '../../containers/NewPost/NewPost';
+import FullPost from '../../containers/FullPost/FullPost';
 import './Blog.css';
 
 class Blog extends Component {
@@ -22,8 +23,11 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        <Route path="/" exact component={Posts} />
-        <Route path="/new-post" component={NewPost} />
+        <Switch>
+          <Route path="/" exact component={Posts} />
+          <Route path="/new-post" component={NewPost} />
+          <Route path="/:id" component={FullPost} />
+        </Switch>
       </div>
     );
   }
--- src/containers/Posts/Posts.js
+++ src/containers/Posts/Posts.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Post from '../../components/Post/Post';
 import axios from '../../axios';
+import { Link } from 'react-router-dom';
 import './Posts.css';
 
 class Posts extends Component {
@@ -40,12 +41,13 @@ class Posts extends Component {
       posts = this.state.posts.map((post) => {
         return (
           <section className="Posts">
-            <Post
-              key={post.id}
-              title={post.title}
-              author={post.author}
-              clicked={() => this.postSelectedHandler(post.id)}
-            />
+            <Link to={'/' + post.id} key={post.id}>
+              <Post
+                title={post.title}
+                author={post.author}
+                clicked={() => this.postSelectedHandler(post.id)}
+              />
+            </Link>
           </section>
         );
       });
